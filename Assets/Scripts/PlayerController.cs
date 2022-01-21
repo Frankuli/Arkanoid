3e81cc261e5c7ce75360c235de4b6e9577e8b965
Franco <franco.mana@hotmail.com>
2022-01-20 21:59:57 -0300

    colors and player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private float inputValue;
+
+    public float speed;
+
+    private Vector2 distance;
+
+
+    void Update()
+    {
+        
+    }
+}
